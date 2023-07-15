b58e24669ece74151c2e2cadb07a926bc4ea2ade
Shingo Tennichi <sngsng.ttt@gmail.com>
2023-07-14 17:34:52 -0700

    created interfaces

---
+++ app/src/main/java/Components/SaveActivityInterface.java
@@ -0,0 +1,7 @@
+package Components;
+
+import android.content.Context;
+
+public interface SaveActivityInterface {
+    abstract void saveActivity(Context context);
+}
---
+++ app/src/main/java/Components/SavePostInterface.java
@@ -0,0 +1,7 @@
+package Components;
+
+import android.content.Context;
+
+public interface SavePostInterface {
+    abstract void savePost(Context context);
+}
